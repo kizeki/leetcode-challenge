ef3ed9c1c0917444c35eae6b8f37c09a415e1faa
kizeki <kizeki.dev@outlook.com>
2025-03-23 03:03:42 +0700

    Solved 2. Add Two Numbers by TS

---
+++ 2-add-two-numbers/2-code.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const arr: number[] = [];
+    let result: ListNode;
+    let flag = 0;
+    do {
+        let digit = (l1?.val || 0) + (l2?.val || 0) + flag;
+        const quotient = digit / 10;
+
+        if (quotient > 1) {
+            digit = digit % 10;
+            flag = 1;
+        } else if (quotient == 1) {
+            digit = 0;
+            flag = 1;
+        } else {
+            flag = 0;
+        }
+
+        arr.push(digit);
+
+        l1 = l1?.next;
+        l2 = l2?.next;
+    }
+    while(l1 || l2)
+
+    if (flag > 0) {
+        arr.push(flag);
+    }
+
+    arr.reverse();
+    arr.forEach(i => {
+        result = new ListNode(i, result);
+    });
+
+    return result;
+};
