22d0635dd6e983c4458135870530fcabeb216812
kizeki <kizeki.dev@outlook.com>
2025-03-23 15:39:05 +0700

    Solved 284. Peeking Iterator by TS

---
+++ 284-peekingiterator/284-code.ts
@@ -0,0 +1,42 @@
+/**
+ * // This is the Iterator's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Iterator {
+ *      hasNext(): boolean {}
+ *
+ *      next(): number {}
+ * }
+ */
+
+class PeekingIterator {
+    iterator: Iterator;
+    currentNumber: number;
+
+    constructor(iterator: Iterator) {
+        this.iterator = iterator;
+        this.currentNumber = this.iterator.next();
+    }
+
+    peek(): number {
+        return this.currentNumber;
+    }
+
+    next(): number {
+        const prevNumber = this.currentNumber;
+        this.currentNumber = this.iterator.next();
+
+        return prevNumber;
+    }
+
+    hasNext(): boolean {
+        return this.currentNumber > 0 || this.iterator?.hasNext() || false;
+    }
+}
+
+/**
+ * Your PeekingIterator object will be instantiated and called as such:
+ * var obj = new PeekingIterator(iterator)
+ * var param_1 = obj.peek()
+ * var param_2 = obj.next()
+ * var param_3 = obj.hasNext()
+ */
